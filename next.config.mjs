c086e7dbcc7de97b774d4de73fefd662c2f51636
JeongTaekCho <cjt3591@gmail.com>
2024-08-22 15:50:24 +0900

    Chore: 프로젝트 셋팅

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        turbo: true,
-    },
+  experimental: {
+    turbo: true,
+  },
 };
 
 export default nextConfig;
